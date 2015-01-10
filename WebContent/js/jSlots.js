b5009f12cd68f4fe372535b4516b02db1cf40259
Greg <greg.kessman@gmail.com>
2015-01-10 15:49:46 -0500

    Add jSlots functionality to webpage. Rough working version.

--- WebContent/js/jSlots.js
+++ WebContent/js/jSlots.js
@@ -49,69 +49,65 @@ function spin() {
     }
 }
 
-function main() {
- coins = document.getElementById('input').value
- document.getElementById('prompt_text').innerHTML = 'Welcome To Slots! Insert Coins';
- document.getElementById('button').setAttribute("onClick", "main()");
- document.getElementById('button').innerHTML = 'Insert';
-
- if (coins == 0){
-  
-  }else{ 
-       document.getElementById('button').setAttribute("onClick", "play()");
-       document.getElementById('button').innerHTML = 'PLAY';
-       document.getElementById('input').value = 0;
-       }
+function intro() {
+    document.getElementById('prompt_text').innerHTML = 'Welcome To Slots! Insert Coins';
+    document.getElementById('button').innerHTML = 'Insert';
+    document.getElementById('button').setAttribute("onClick", "main()");
 }
 
+function main() {
+    coins = document.getElementById('input').value
+    document.getElementById('coins').innerHTML = coins;
+    document.getElementById('prompt_text').innerHTML = 'Please place bet below';  
+    document.getElementById('button').innerHTML = 'PLAY';
+    document.getElementById('input').value = 0;
+    document.getElementById('button').setAttribute("onClick", "play()");
+}
 
 function play() {
-   console.log(" in play function");
-   bet = document.getElementById('input').value;
-   if (bet == 0 || bet == null ){
-     document.getElementById('prompt_text').innerHTML = 'Please place initial bet below';  
-   }else{
-
+    bet = document.getElementById('input').value;
+    coins = coins - bet
     win = false;
     winRow = [];
     payout = 0;
- 
+    document.getElementById('coins').innerHTML = coins;
     spin();
-    //show results of spin
-    console.log(" ");
-    console.log(coins + " - " + bet + " = " + (coins = coins - bet) + " coins left");
-    for (var i = 0; i < 3; i++) {
-        if ((sOut[i][0].id === sOut[i][1].id) && (sOut[i][1].id === sOut[i][2].id)) {
-            console.log("-----------------------------");
-            console.log("| " + sOut[i][0].name + "|" + sOut[i][1].name + "|" + sOut[i][2].name + "|  WINNER!");
-            win = true;
-            winRow.push(i);
-        } else {
-            console.log("-----------------------------");
-            console.log("| " + sOut[i][0].name + "|" + sOut[i][1].name + "|" + sOut[i][2].name + "|");
-        }        
-    }
-    console.log("-----------------------------");
-    console.log(" ");
-    
+    validateResults();
+
     if (win) {
         for (i = 0; i < winRow.length; i++) {
             payout = payout + (bet * sOut[(winRow[i])][1].value);       
         }
 		coins = coins + payout;
-        console.log("Total Payout: " + payout + " | Total Coins: " + coins + " coins");
+        document.getElementById('coins').innerHTML = coins;
+        document.getElementById('winnings_prompt').innerHTML = 'HOORAY! You Won ' + payout + ' Coins!'; 
     }   
-  }
 }
 
-main();
 
+function validateResults() {
+    //show results of spin
+    for (var i = 0; i < 3; i++) {
+        if ((sOut[i][0].id === sOut[i][1].id) && (sOut[i][1].id === sOut[i][2].id)) {
+            document.getElementById(i + '_0').innerHTML = sOut[i][0].name;
+            document.getElementById(i + '_1').innerHTML = sOut[i][1].name;
+            document.getElementById(i + '_2').innerHTML = sOut[i][2].name;
+            win = true;
+            winRow.push(i);
+        } else {
+            document.getElementById(i + '_0').innerHTML = sOut[i][0].name;
+            document.getElementById(i + '_1').innerHTML = sOut[i][1].name;
+            document.getElementById(i + '_2').innerHTML = sOut[i][2].name;
+            document.getElementById('winnings_prompt').innerHTML = 'Sorry, please try again!'; 
+        }        
+    }
+}
+
+intro();
 
 if (coins <= 0) {
     console.log("You have no more coins, sorry!");
 } else if (bet === 'no' || bet === null){
     console.log("Thanks for playing!");
 }
-
-
 
